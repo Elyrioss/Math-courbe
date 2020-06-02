aecc836abaaca0d7e8d267f888089dc0cd49b764
Esteban <esteban.dumas@free.fr>
2020-06-02 17:43:24 +0200

    koko

--- Assets/Script/Kobbelt.cs
+++ Assets/Script/Kobbelt.cs
@@ -7,45 +7,61 @@ public class Kobbelt : MonoBehaviour
     // Start is called before the first frame update
     public GameObject plot;
     public GameObject plot1;
+    public GameObject plot2;
     public List<Triangle> tri=new List<Triangle>();
+    
     void Start()
     {
        Vertice v0 = new Vertice(new Vector3(0,0,0));
-       Vertice v1 = new Vertice(new Vector3(7,-1,0));
-       Vertice v2 = new Vertice(new Vector3(4,3,0));
-       Vertice v3 = new Vertice(new Vector3(3,9,0));
-       Vertice v4 = new Vertice(new Vector3(-3,5,0));
-       Vertice v5 = new Vertice(new Vector3(-7,1,0));
+       Vertice v1 = new Vertice(new Vector3(8f,0,0));
+       Vertice v2 = new Vertice(new Vector3(4f,0,6f));
+       Vertice v3 = new Vertice(new Vector3(4f,6f,3f));
        
-       v0.AddNeighbours(new List<Vertice>(){v1,v2,v4,v5});
-       v1.AddNeighbours(new List<Vertice>(){v0,v2});
-       v2.AddNeighbours(new List<Vertice>(){v0,v1,v3,v4});
-       v3.AddNeighbours(new List<Vertice>(){v2,v4});     
-       v4.AddNeighbours(new List<Vertice>(){v0,v2,v3,v5});
-       v4.AddNeighbours(new List<Vertice>(){v0,v4});
-
+       v0.AddNeighbours(new List<Vertice>(){v1,v2,v3});
+       v1.AddNeighbours(new List<Vertice>(){v0,v2,v3});
+       v2.AddNeighbours(new List<Vertice>(){v0,v1,v3});
+       v3.AddNeighbours(new List<Vertice>(){v0,v1,v2});     
+                
        Triangle t0 = new Triangle(new List<Vertice>() {v0, v1, v2});
-       Triangle t1 = new Triangle(new List<Vertice>() {v0, v2, v4});
-       Triangle t2 = new Triangle(new List<Vertice>() {v0, v4, v5});
-       Triangle t3 = new Triangle(new List<Vertice>() {v2, v3, v4});
+       Triangle t1 = new Triangle(new List<Vertice>() {v0, v1, v3});
+       Triangle t2 = new Triangle(new List<Vertice>() {v1, v2, v3});
+       Triangle t3 = new Triangle(new List<Vertice>() {v0, v2, v3});
 
-       t0.AddNeighbour(t1);
+       t0.AddNeighbours(new List<Triangle>(){t1,t2,t3});
        t1.AddNeighbours(new List<Triangle>(){t0,t2,t3});
-       t2.AddNeighbour(t1);
-       t3.AddNeighbour(t1);
-
-       Debug.Log(t0.Vertices.Count);
+       t2.AddNeighbours(new List<Triangle>(){t0,t1,t3});
+       t3.AddNeighbours(new List<Triangle>(){t0,t1,t2});
        
        t0.Center = TriCenter(t0);
        t1.Center = TriCenter(t1);
        t2.Center = TriCenter(t2);
        t3.Center = TriCenter(t3);
        
-       tri.Add(t0);
-       tri.Add(t1);
-       tri.Add(t2);
-       tri.Add(t3);
-       ShowShape();
+       List<Vertice> vertices = new List<Vertice>(){v0,v1,v2,v3};
+       List<Vertice> basic = new List<Vertice>(){v0,v1,v2,v3};
+       vertices = Perturbator(vertices); 
+       
+       Triangle T0 = new Triangle(new List<Vertice>() {vertices[0], vertices[1], vertices[2]});
+       Triangle T1 = new Triangle(new List<Vertice>() {vertices[0], vertices[1], vertices[3]});
+       Triangle T2 = new Triangle(new List<Vertice>() {vertices[1], vertices[2], vertices[3]});
+       Triangle T3 = new Triangle(new List<Vertice>() {vertices[0], vertices[2], vertices[3]});
+ 
+       T0.AddNeighbours(new List<Triangle>(){T1,T2,T3});
+       T1.AddNeighbours(new List<Triangle>(){T0,T2,T3});
+       T2.AddNeighbours(new List<Triangle>(){T0,T1,T3});
+       T3.AddNeighbours(new List<Triangle>(){T0,T1,T2});
+       
+       T0.Center = t0.Center;
+       T1.Center = t1.Center;
+       T2.Center = t2.Center;
+       T3.Center = t3.Center;
+       
+       tri.Add(T0);
+       tri.Add(T1);
+       tri.Add(T2);
+       tri.Add(T3);
+       
+       ShowShape(vertices,basic);
     }
 
     public Vertice TriCenter(Triangle t)
@@ -56,81 +72,45 @@ public class Kobbelt : MonoBehaviour
         return new Vertice((v1.Position+v2.Position+v3.Position)/3,new List<Vertice>(){v1,v2,v3});      
     }
 
-    public void ShowShape()
+    public List<Vertice> Perturbator(List<Vertice> vertices)
     {
-        foreach (Triangle triangle in tri)
+        List<Vertice> Result = vertices;
+        
+        float alpha = 0;
+        Vector3 V;
+        for (int i = 0; i < vertices.Count; i++)
         {
-            foreach (Vertice v in triangle.Vertices)
+            alpha = (1f / 9f) * (4f - 2f * Mathf.Cos((2f*Mathf.PI) / vertices[i].Neighbours.Count));
+
+            Vector3 sum = new Vector3(0,0,0);
+            for (int j = 0; j < vertices[i].Neighbours.Count; j++)
             {
-                Instantiate(plot, v.Position, Quaternion.identity);
+                sum += vertices[i].Neighbours[j].Position;
             }
-            DrawLine(triangle.Vertices[0].Position, triangle.Vertices[1].Position,Color.white,1000);
-            DrawLine(triangle.Vertices[1].Position, triangle.Vertices[2].Position,Color.white,1000);
-            DrawLine(triangle.Vertices[2].Position, triangle.Vertices[0].Position,Color.white,1000);
+            Result[i] = new Vertice(vertices[i].Position * (1f - alpha) + (alpha / vertices[i].Neighbours.Count) * sum);
+        }
+
+        return Result;
+    }
+
+    public void ShowShape(List<Vertice> perturbed,List<Vertice> basic)
+    {
+        
+        foreach (Vertice v in basic)
+        {
+            Instantiate(plot, v.Position, Quaternion.identity);
+        }
+        
+        foreach (Triangle triangle in tri)
+        {          
+            
+            //DrawLine(triangle.Vertices[0].Position, triangle.Vertices[1].Position,Color.white,1000);
+            //DrawLine(triangle.Vertices[1].Position, triangle.Vertices[2].Position,Color.white,1000);
+            //DrawLine(triangle.Vertices[2].Position, triangle.Vertices[0].Position,Color.white,1000);
             
             DrawLine(triangle.Center.Position, triangle.Vertices[1].Position,Color.blue,1000);
             DrawLine(triangle.Center.Position, triangle.Vertices[2].Position,Color.blue,1000);
-            DrawLine(triangle.Center.Position, triangle.Vertices[0].Position,Color.blue,1000);
-
-            bool a = true;
-            foreach (Triangle triangle2 in tri)
-            {
-                if (triangle2 == triangle)
-                {
-                    continue;
-                }
-                
-                if (triangle2.Vertices.Contains(triangle.Vertices[1]) && triangle2.Vertices.Contains(triangle.Vertices[2]))
-                {
-                    a = false;
-                }
-            }
-
-            if (a)
-            {
-                Vector3 Lerped = Vector3.Lerp(triangle.Vertices[1].Position, triangle.Vertices[2].Position, 0.5f);
-                DrawLine(triangle.Center.Position, Lerped+(Lerped-triangle.Center.Position)*2,Color.green,1000);
-            }
-            
-            a = true;
-            foreach (Triangle triangle2 in tri)
-            {
-                if (triangle2 == triangle)
-                {
-                    continue;
-                }
-                
-                if (triangle2.Vertices.Contains(triangle.Vertices[0]) && triangle2.Vertices.Contains(triangle.Vertices[2]))
-                {
-                    a = false;
-                }
-            }
-
-            if (a)
-            {
-                Vector3 Lerped2 = Vector3.Lerp(triangle.Vertices[0].Position, triangle.Vertices[2].Position, 0.5f);
-                DrawLine(triangle.Center.Position, Lerped2+(Lerped2-triangle.Center.Position)*2,Color.green,1000);
-            }
-            
-            a = true;
-            foreach (Triangle triangle2 in tri)
-            {
-                if (triangle2 == triangle)
-                {
-                    continue;
-                }
-                
-                if (triangle2.Vertices.Contains(triangle.Vertices[0]) && triangle2.Vertices.Contains(triangle.Vertices[1]))
-                {
-                    a = false;
-                }
-            }
-
-            if (a)
-            {
-                Vector3 Lerped3 = Vector3.Lerp(triangle.Vertices[1].Position, triangle.Vertices[0].Position, 0.5f);
-                DrawLine(triangle.Center.Position, Lerped3+(Lerped3-triangle.Center.Position)*2,Color.green,1000);            
-            }
+            DrawLine(triangle.Center.Position, triangle.Vertices[0].Position,Color.blue,1000);     
 
             Instantiate(plot1, triangle.Center.Position, Quaternion.identity);
             
@@ -139,6 +119,12 @@ public class Kobbelt : MonoBehaviour
                 DrawLine(triangle.Center.Position, TN.Center.Position,Color.blue,1000);
             }
             
+        }
+
+        foreach (Vertice p in perturbed)
+        {
+            Debug.Log(p.Position);
+            Instantiate(plot2, p.Position, Quaternion.identity);
         }
     }
     

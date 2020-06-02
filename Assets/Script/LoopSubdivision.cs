0548e4d0371d37519cb4a05b06a3c61a77d22b24
Tony <m.kaddour@sowhen.fr>
2020-06-02 11:41:26 +0200

    Adding loop subdiv scripts

---
+++ Assets/Script/LoopSubdivision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Subdiv
+{
+    [RequireComponent (typeof(MeshFilter))]
+    public class LoopSubdivision : MonoBehaviour
+    {
+        [SerializeField, Range(1, 4)] protected int details = 1;
+        [SerializeField] bool weld = false;
+
+        void Start()
+        {
+            var filter = GetComponent<MeshFilter>();
+            var source = filter.mesh;
+            var mesh = SubdivisionSurface.Subdivide(SubdivisionSurface.Weld(source, float.Epsilon, source.bounds.size.x), details, weld);
+            filter.sharedMesh = mesh;
+        }
+    
+    }
+
+}    
+
